bd841243d3d25fd0df21b1e220d98446033cf2ae
Josh Palmer <91572133+josh-palmerk@users.noreply.github.com>
2024-06-08 14:42:40 -0600

    Initial commit

---
+++ yourlife/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=140)
+    content = models.TextField(max_length=10000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
